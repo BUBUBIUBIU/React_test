3154a76d183a3c03128c1e4a6417c042c7da8702
Michael Luo <shaochuanl@student.unimelb.edu.au>
2019-10-06 14:43:38 +1100

    Finish and basic features of React syntax

--- src/App.js
+++ src/App.js
@@ -14,19 +14,38 @@ class App extends Component {
      otherState: 'some other value'
   }
 
-  switchNameHandlee = () => {
+  switchNameHandler = (newName) => {
     // console.log('Was clicked!');
     // DON'T DO THIS this.state.persons[0].name = 'Maximilian'; (不能直接用=改变state里的东西)
     this.setState({
       persons: [
-        {name: 'Maximilian', age: '28'},
+        {name: newName, age: '28'},
         {name: 'Manu', age: '29'},
         {name: 'Stephanie', age: '27'}
       ]
     })
   }
 
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: 'Stephanie', age: 26 }
+      ]
+    })
+  }
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      boder: '1px solid blue',
+      padding: '8px',
+      // 这个属性让鼠标hover在按钮上时呈手状
+      cursor: 'pointer'
+    }
+
     return (
       // 这看上去是HTML，其实是JSX
       <div className="App">
@@ -34,10 +53,20 @@ class App extends Component {
         <p>This is really working!</p>
         {/* onClick里的method千万不要加(),我们要的只是method的reference, 不然的话在react
         render这段jsx代码的时候会直接call这个method */}
-        <button onClick={this.switchNameHandlee}>Switch Name</button> 
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My Hobbies: Racing</Person> 
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button 
+          style = {style}
+          onClick={() => this.switchNameHandler('Maximilian')}>Switch Name</button> 
+        <Person 
+        name={this.state.persons[0].name} 
+        age={this.state.persons[0].age} />
+        <Person 
+        name={this.state.persons[1].name} 
+        age={this.state.persons[1].age} 
+        click={this.switchNameHandler.bind(this, 'Max!')}
+        changed={this.nameChangedHandler}>My Hobbies: Racing</Person> 
+        <Person 
+        name={this.state.persons[2].name} 
+        age={this.state.persons[2].age}/>
       </div>
     );
     // return React.createElement('div', null, React.createElement('h1', null, 'Does it work now?'));
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import './Person.css';
 
 // 这里用const，我们不希望这个function被更改
 const person = (props) => {
     return (
-        <div>
-            <p>I'm a {props.name} and I am {props.age} years old!</p>
+        <div className='Person'>
+            <p onClick={props.click}>I'm a {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
+            {/* value attribute 就是field里自动出现的default选项 */}
+            <input type='text' onChange={props.changed} value={props.name} />
         </div>    
     ) 
 }
