47c0c5f22420a6cb7741a73028dcaff7a16137d5
Michael Luo <shaochuanl@student.unimelb.edu.au>
2019-10-07 16:40:04 +1100

    Complete the conditional output and output list.

--- src/App.js
+++ src/App.js
@@ -7,14 +7,15 @@ class App extends Component {
 
   state = {
      persons: [
-       {name: 'Max', age: '28'},
-       {name: 'Manu', age: '29'},
-       {name: 'Stephanie', age: '26'}
+       { id: 'asfa1', name: 'Max', age: '28'},
+       { id: 'vasdf1', name: 'Manu', age: '29'},
+       { id: 'asdf11', name: 'Stephanie', age: '26'}
      ],
-     otherState: 'some other value'
+     otherState: 'some other value',
+     showPersons: false
   }
 
-  switchNameHandler = (newName) => {
+  /**  switchNameHandler = (newName) => {
     // console.log('Was clicked!');
     // DON'T DO THIS this.state.persons[0].name = 'Maximilian'; (不能直接用=改变state里的东西)
     this.setState({
@@ -24,16 +25,44 @@ class App extends Component {
         {name: 'Stephanie', age: '27'}
       ]
     })
+  } */
+
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    // We cannot mutate the state, ... do the same thing to object like to array (也可以拆散object)
+    // 方法一
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    // 方法二
+    // const person = Object.assign({}, this.state.persons[personIndex]);
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person; 
+
+    this.setState({ persons: persons })
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'Max', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 26 }
-      ]
-    })
+  deletePersonHandler = (personIndex) => {
+    // 这里再次解释一下更改const的原因，这个persons里其实是指向array的reference，我们本质上没有更改persons
+    // 这里我们不提倡用splice，因为它会直接操作state里的property，我们可以使用另外两种方法
+    // 在不改变state的情况下完成任务
+    // const persons = this.state.persons.slice(); (方法1)
+    const persons = [...this.state.persons];  // (方法2，利用ES6新特性) 
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
+  }
+
+  // 这里提了一下arrow function中this key word的功能的不一样
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
   }
 
   render() {
@@ -46,6 +75,25 @@ class App extends Component {
       cursor: 'pointer'
     }
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      // 神奇的一幕，只要一个括号就能让代码变成JSX
+      persons = (
+        <div>
+          {/* tips， arrow function 有两个arguments时要用小括号括起来 */}
+          {this.state.persons.map((person, index) => {
+            return <Person 
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age}
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)} />
+          })}
+        </div> 
+      );
+    }
+
     return (
       // 这看上去是HTML，其实是JSX
       <div className="App">
@@ -55,18 +103,8 @@ class App extends Component {
         render这段jsx代码的时候会直接call这个method */}
         <button 
           style = {style}
-          onClick={() => this.switchNameHandler('Maximilian')}>Switch Name</button> 
-        <Person 
-        name={this.state.persons[0].name} 
-        age={this.state.persons[0].age} />
-        <Person 
-        name={this.state.persons[1].name} 
-        age={this.state.persons[1].age} 
-        click={this.switchNameHandler.bind(this, 'Max!')}
-        changed={this.nameChangedHandler}>My Hobbies: Racing</Person> 
-        <Person 
-        name={this.state.persons[2].name} 
-        age={this.state.persons[2].age}/>
+          onClick={this.togglePersonsHandler}>Toggle Persons</button> 
+          {persons}
       </div>
     );
     // return React.createElement('div', null, React.createElement('h1', null, 'Does it work now?'));
